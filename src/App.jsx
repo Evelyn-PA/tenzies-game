70099bd2392a4f48cb2519a3cc9cb498232086d0
Anh Nguyen <phuonganhnguyen6165@gmail.com>
2025-07-16 13:09:29 +0300

    completed this project

--- src/App.jsx
+++ src/App.jsx
@@ -53,12 +53,11 @@ export default function App() {
 
   return (
     <main>
-      <Analytics />
-
       <h1>Tenzies Game</h1>
       <p className="instructions">Roll until all dice are the same.<br />Click each dice to freeze it at its current value between rolls.</p>
       <div className="container">
         {diceElement}
+        <Analytics />
       </div>
       {gameWon && <Confetti />}
       <button ref={buttonRef} className="roll-button" onClick={getRoll}>{gameWon ? "New Game" : "Roll Dice"}</button>
